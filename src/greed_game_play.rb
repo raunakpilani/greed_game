9aebdd78b5d40b8629b7f5be57f22af0a45cbb87
Raunak Pilani <raunak@browserstack.com>
2013-08-27 15:02:54 +0530

    replacing tabs with spaces

--- src/greed_game_play.rb
+++ src/greed_game_play.rb
@@ -3,8 +3,8 @@ gm = Game.new(5)
 gm.player_standings
 
 until gm.game_over do
-	puts
-	puts gm.next_turn
-	print "Any key to continue: "
-	gets
+  puts
+  puts gm.next_turn
+  print "Any key to continue: "
+  gets
 end
